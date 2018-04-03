7c7399f566bfe22ea08027423749483620fbd65a
celeryrobo <liuqing@yydrobo.com>
2018-04-03 14:30:10 +0800

    commit code

--- src/main/java/com/yyd/service/common/ExternalSemanticServiceImpl.java
+++ src/main/java/com/yyd/service/common/ExternalSemanticServiceImpl.java
@@ -4,10 +4,15 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.logging.Logger;
 
+import com.ybnf.compiler.beans.AbstractSemanticResult.Operation;
+import com.ybnf.compiler.beans.AbstractSemanticResult.ParamType;
 import com.ybnf.compiler.beans.YbnfCompileResult;
 import com.ybnf.semantic.Semantic;
 import com.ybnf.semantic.SemanticContext;
+import com.yyd.external.semantic.ExternalCommonBean;
 import com.yyd.external.semantic.ExternalSemanticResult;
+import com.yyd.external.semantic.ExternalSemanticResult.OperationEx;
+import com.yyd.external.semantic.ExternalSemanticResult.ParamTypeEx;
 import com.yyd.external.semantic.ExternalSemanticService;
 
 public class ExternalSemanticServiceImpl implements Semantic<CommonBean> {
@@ -63,7 +68,57 @@ public class ExternalSemanticServiceImpl implements Semantic<CommonBean> {
 		if (intent != null) {
 			yydSlots.put("intent", intent);
 		}
-		bean.setText(semanticResult.getAnswer());
+		ExternalCommonBean exBean = semanticResult.getData();
+		String text = null;
+		if (exBean != null) {
+			text = exBean.getText();
+			bean.setUrl(exBean.getUrl());
+			bean.setOperation(convertOperation(semanticResult.getOperation()));
+			bean.setParamType(convertParamType(semanticResult.getParamType()));
+		}
+		if (text == null) {
+			text = semanticResult.getAnswer();
+		}
+		bean.setText(text);
+		bean.setResource(semanticResult.getResource());
 		return bean;
+	}
+
+	private Operation convertOperation(OperationEx operEx) {
+		switch (operEx) {
+		case PLAY:
+			return Operation.PLAY;
+		case APP:
+			return Operation.APP;
+		case MOVE:
+		case CONTROL:
+		case COMMAND:
+			return Operation.COMMAND;
+		case SPEAK:
+		default:
+			return Operation.SPEAK;
+		}
+	}
+
+	private ParamType convertParamType(ParamTypeEx paramTypeEx) {
+		switch (paramTypeEx) {
+		case U:
+			return ParamType.U;
+		case TU:
+			return ParamType.TU;
+		case IT:
+			return ParamType.IT;
+		case IU:
+			return ParamType.IU;
+		case ITU:
+			return ParamType.ITU;
+		case C:
+			return ParamType.C;
+		case TC:
+			return ParamType.TC;
+		case T:
+		default:
+			return ParamType.T;
+		}
 	}
 }
